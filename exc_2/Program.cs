3307dc44929cfff1b7e8e018577e4be2b09278c9
Slava <slavanew98@mail.ru>
2023-01-07 00:04:00 +0300

    2nd exercise - completed

---
+++ exc_2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+Выполнить с помощью рекурсии.
+*/
+
+//тело программы
+
+Console.Write("Введите число M: ");
+int M = InputInt();
+
+Console.Write("Введите число N: ");
+int N = InputInt();
+
+SumNaturalNumbers(M, N);
+
+
+//методы
+
+int InputInt()
+{
+    bool isParse = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isParse)
+    {
+        return number;
+    }
+    else
+    {
+        throw new ArgumentException("Введено некорректное значение");
+    }
+}
+
+void SumNaturalNumbers(int M, int N, int sum = 0)
+{
+    if(N < M)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+
+    sum = sum + N;
+    SumNaturalNumbers(M, N - 1, sum);
+}
