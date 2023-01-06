71e765b796e9a95a677f4fd30a839bdafd279c82
Slava <slavanew98@mail.ru>
2023-01-06 23:50:02 +0300

    Init commit with 1st exercise

---
+++ exc_1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+*/
+
+//тело программы
+
+Console.Write("Введите число: ");
+int N = InputInt();
+
+Console.Write("натуральные числа: ");
+NaturalNumber(N);
+
+
+//методы
+
+int InputInt()
+{
+    bool isParse = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isParse)
+    {
+        return number;
+    }
+    else
+    {
+        throw new ArgumentException("Введено некорректное значение");
+    }
+}
+
+void NaturalNumber(int i)
+{
+    if (i == 0)
+    {
+        return;
+    }
+    Console.Write($"{i}   ");
+    NaturalNumber(i - 1);
+}
